3f562683bd23e8f3bf8bb26114c1c4cf2d273e2c
Josh Possel <josh@possel.net>
2017-11-07 11:15:44 -0600

    File hosting for Resume

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response, send_file
 app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Welcome to possel.net!"
+    return '''
+    	<html><body>
+    	Welcome to possel.net! <a href="/getFile"> Click</a>
+    	'''
+
+#@app.route("/getFile")
+#def getFile():
+#	return send_file
+	
